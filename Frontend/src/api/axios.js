529be7d54d89f5547b993f984f4f8d0620873ae0
akash-shit <akashshit541@gmail.com>
2026-08-04 20:46:14 +0530

    Update backend changes

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://jupiter-bank.onrender.com/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://jupiter-bank.onrender.com/api/auth",
+  //baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
