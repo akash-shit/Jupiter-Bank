937a9ffb2648b3a41dfde50d303037f83afb1db7
akash-shit <akashshit541@gmail.com>
2026-08-06 15:09:34 +0530

    Fix NotFound import path

--- Frontend/src/pages/Dashboard/NotFound.jsx
+++ Frontend/src/pages/Dashboard/NotFound.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
+
 
 function NotFound() {
   const { user } = useAuth();
